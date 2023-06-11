eb8fa2d3663282905ad2dc90a0fcb445565b4cca
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-06-11 01:36:25 -0700

    Implement advanced logging feature with category, severity levels, and colored output

--- TBG/DebugUtils.cpp
+++ TBG/DebugUtils.cpp
@@ -1,8 +1,25 @@
 #include "DebugUtils.h"
 #include <iostream>
 
-namespace DebugUtils {
-	void Log(std::string message) {
-		std::cout << message << std::endl;
+namespace DebugUtils
+{
+	void Log(const std::string &category, Severity severity, const std::string &message)
+	{
+		switch (severity)
+		{
+		case Info:
+			std::cout << "[" << category << "] "
+					  << "[INFO] ";
+			break;
+		case Warning:
+			std::cout << YELLOW << "[" << category << "] "
+					  << "[WARNING] ";
+			break;
+		case Error:
+			std::cout << RED << "[" << category << "] "
+					  << "[ERROR] ";
+			break;
+		}
+		std::cout << message << RESET << std::endl;
 	}
 }
--- TBG/DebugUtils.h
+++ TBG/DebugUtils.h
@@ -1,9 +1,35 @@
 #pragma once
 
+// the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
 #include <string>
+#define RESET "\033[0m"
+#define BLACK "\033[30m"			  /* Black */
+#define RED "\033[31m"				  /* Red */
+#define GREEN "\033[32m"			  /* Green */
+#define YELLOW "\033[33m"			  /* Yellow */
+#define BLUE "\033[34m"				  /* Blue */
+#define MAGENTA "\033[35m"			  /* Magenta */
+#define CYAN "\033[36m"				  /* Cyan */
+#define WHITE "\033[37m"			  /* White */
+#define BOLDBLACK "\033[1m\033[30m"	  /* Bold Black */
+#define BOLDRED "\033[1m\033[31m"	  /* Bold Red */
+#define BOLDGREEN "\033[1m\033[32m"	  /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m"  /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m"	  /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m"	  /* Bold Cyan */
+#define BOLDWHITE "\033[1m\033[37m"	  /* Bold White */
+// Note: If you don't use RESET the color will remain changed until the next time you use a color code.
+// source: https://stackoverflow.com/questions/9158150/colored-output-in-c
 
 namespace DebugUtils
 {
-	void Log(std::string message);
+	enum Severity
+	{
+		Info,
+		Warning,
+		Error
+	};
+	void Log(const std::string &category, Severity severity, const std::string &message);
 }
 
